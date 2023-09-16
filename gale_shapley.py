9f0e4f959745b9beb5d2ccb9790979a6900f36b3
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2023-09-17 01:24:38 +0530

    Functional implementation of the Gale Shapley Algorithm completed

--- gale_shapley.py
+++ gale_shapley.py
@@ -9,16 +9,19 @@ class Man:
         self.name = name
         self.prefLis = prefLis
         self.matched = matched
+        self.prop_List = []
 
     def updatePref(self,l1):
         self.prefLis = l1
         return 
+
         
 class Woman:
     def __init__(self,name,prefLis=[],matched=None):
         self.name = name 
         self.prefLis = prefLis
         self.matched = matched 
+        self.prop_List = []
 
     def updatePref(self, l2):
         self.prefLis = l2
@@ -40,12 +43,15 @@ m4 = Man("Man 4",[],None)
 womenList = [w1,w2,w3,w4]
 menList = [m1,m2,m3,m4]
 
+
+stable_match = []
+
+
 for i in range(len(menList)):
     menList[i].updatePref(random.sample(womenList, len(womenList)))
     womenList[i].updatePref(random.sample(menList, len(menList)))
 
-print("Men list: ")
-print("\n")
+
 
 for j in range(len(menList)):
     print(menList[j].name,"-> ",end=' ') 
@@ -53,10 +59,7 @@ for j in range(len(menList)):
         print(k.name, end=",")
     print("")
 
-print("\n")
 
-print("Women list: ")
-print("\n")
 for m in range(len(womenList)):
     print(womenList[m].name,"->", end=' ')
     for l in (womenList[m].prefLis):
@@ -64,4 +67,46 @@ for m in range(len(womenList)):
     print("")
 
 
-# def galeShapley(menList,womenList):
+def isAllMatch(l1):
+    match = True 
+    for k in l1:
+        if k.matched is not None:
+            match = match and True 
+        else:
+            match = match and False 
+
+    return match 
+
+
+def propose(man1,woman1):
+    if woman1.matched is None:
+        woman1.matched = man1
+        man1.matched = woman1 
+    else:
+        woman1_curr = woman1.matched 
+        wom_pref = woman1.prefLis
+        if wom_pref.index(woman1_curr) < wom_pref.index(man1):
+            woman1.matched = woman1_curr
+            propose(man1,man1.prefLis[man1.prefLis.index(woman1) + 1])
+        else: 
+            woman1_curr.matched = None 
+            woman1.matched = man1
+            man1.matched = woman1
+            propose(woman1_curr,woman1_curr.prefLis[woman1_curr.prefLis.index(woman1) + 1])
+        
+
+def gale_shapley(mList,wList):
+    if len(mList) != len(wList):
+        print("Not acceptable")
+        
+    else:
+        for k in mList:
+            propose(k, k.prefLis[0])
+
+        for m in mList:
+            tupel = (m.name, m.matched.name)
+            stable_match.append(tupel)
+
+
+gale_shapley(menList,womenList)
+print(stable_match)
