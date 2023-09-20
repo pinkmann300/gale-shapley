a2a2f0be755c2ec6e3d7abe3dde3b4d5ca2378f8
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2023-09-21 00:25:36 +0530

    Modified code to handle occurences of no match

--- gale_shapley.py
+++ gale_shapley.py
@@ -2,6 +2,7 @@
 # A definition of the stable matching problem can be found in the "Algorithms Design" book by Eva Tardos. 
 
 
+
 import random 
 
 class Man:
@@ -40,30 +41,39 @@ m2 = Man("Man 2",[],None)
 m3 = Man("Man 3",[],None)
 m4 = Man("Man 4",[],None)
 m5 = Man("Man 5",[],None)
+m6 = Man("Man 6", [], None)
+m7 = Man("Man 7", [], None)
+m8 = Man("Man 8", [], None)
 
 womenList = [w1,w2,w3,w4,w5]
-menList = [m1,m2,m3,m4,m5]
+menList = [m1,m2,m3,m4,m5,m6,m7,m8]
+
+
+# Writing code to test out the single man condition and making appropriate changes to the "propose" function to handle the same.
 
 for i in range(len(menList)):
-    menList[i].updatePref(random.sample(womenList, len(womenList)))
-    womenList[i].updatePref(random.sample(menList, len(menList)))
+    menList[i].updatePref(random.sample(womenList,len(womenList)))
+
+for j in range(len(womenList)):
+    womenList[j].updatePref(random.sample(menList, len(menList)))
+
 
 
 print("Men's preference list: ")
 print("\n")
-for j in range(len(menList)):
-    print(menList[j].name,"-> ",end=' ') 
-    for k in (menList[j].prefLis):
+for l in range(len(menList)):
+    print(menList[l].name,"-> ",end=' ') 
+    for k in (menList[l].prefLis):
         print(k.name, end=",")
     print("")
 
 print("\n")
 print("Women's preference list: ")
 print("\n")
-for m in range(len(womenList)):
-    print(womenList[m].name,"->", end=' ')
-    for l in (womenList[m].prefLis):
-        print(l.name, end=',')
+for r in range(len(womenList)):
+    print(womenList[r].name,"->", end=' ')
+    for c in (womenList[r].prefLis):
+        print(c.name, end=',')
     print("")
 
 print("\n")
@@ -89,26 +99,31 @@ def propose(man1,woman1):
         wom_pref = woman1.prefLis
         if wom_pref.index(woman1_curr) < wom_pref.index(man1):
             woman1.matched = woman1_curr
-            propose(man1,man1.prefLis[man1.prefLis.index(woman1) + 1])
+            if man1.prefLis.index(woman1) + 1 > (len(man1.prefLis) - 1):
+                man1.matched = None
+            else:
+                propose(man1,man1.prefLis[man1.prefLis.index(woman1) + 1])
+
         else: 
             woman1_curr.matched = None 
             woman1.matched = man1
             man1.matched = woman1
-            propose(woman1_curr,woman1_curr.prefLis[woman1_curr.prefLis.index(woman1) + 1])
+            if woman1_curr.prefLis.index(woman1) + 1 > (len(woman1_curr.prefLis) -1):
+                woman1_curr.matched = None
+            else:
+                propose(woman1_curr,woman1_curr.prefLis[woman1_curr.prefLis.index(woman1) + 1])
         
 
 def gale_shapley(mList,wList):
     stable_match = []
-    if len(mList) != len(wList):
-        print("Not acceptable")
-        
-    else:
-        for k in mList:
-            propose(k, k.prefLis[0])
+    for k in mList:
+        propose(k, k.prefLis[0])
             
-        for m in mList:
-            tupel = (m.name, m.matched.name)
-            stable_match.append(tupel)
+    for m in mList:
+        if m.matched is None:
+            m.matched = Man("None", [], None)
+        tupel = (m.name, m.matched.name)
+        stable_match.append(tupel)
 
     return stable_match
 
